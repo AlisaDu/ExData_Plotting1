b8ab238169fc3c39346c7358e480c34ae1a6b940
Alisa Dudinova <alisa.dudinova@ace.ee>
2020-11-16 15:05:11 +0200

    all graphs and finished script

--- rscript.R
+++ rscript.R
@@ -20,7 +20,6 @@ data$Sub_metering_2 <- as.numeric(data$Sub_metering_2)
 #adding a column to data frame with weekday
 data$Weekday <- weekdays.Date(data$Timestamp, abbreviate = TRUE)
 
-print(head(data))
 
 # erase rawdata data frame to free up resources
 rm (rawdata)
@@ -28,29 +27,111 @@ rm (rawdata)
 #assigning label defaults
 gap.label <- "Global Active Power (kilowatts)"
 
+# constructor function for plot device
+new.plot <- function (filename){
+    png(filename = filename, width = 480, height = 480, units= "px")
+}
+
 # constructing first plot
-png ("plot1.png", width = 480, height = 480, units= "px")
-with(data, hist(Global_active_power, 
+
+build.plot1 <- function(){
+ with(data, hist(Global_active_power, 
                           col = "red",
                           main = "Global Active Power", 
                           xlab = gap.label))
+}
 
-dev.off()
 
 # constructing second plot
-png ("plot2.png", width = 480, height = 480, units= "px")
-with(data, plot(Timestamp, 
+build.plot2 <- function(){
+ with(data, plot(Timestamp, 
                 Global_active_power,
                 ylab= gap.label,
                 xlab= "",
                 type = "n"))
-with(data, lines(Timestamp, 
+ with(data, lines(Timestamp, 
                  Global_active_power, 
                  type = "l", 
                  lty = 1))
+}
 
-# dev.copy(device = png, "plot2.png")
-dev.off()
 
 # constructing third plot
+build.plot3 <- function(legend.border){
+    with(data, plot(Timestamp,
+                    Sub_metering_1, 
+                    type = "n",
+                    ylab = "Energy sub metering",
+                    xlab = ""))
+    with(data, lines(Timestamp, 
+                     Sub_metering_1, 
+                     type = "l", 
+                     lty = 1, 
+                     col= "black"))
+    with(data, lines(Timestamp, 
+                     Sub_metering_2, 
+                     type = "l", 
+                     lty = 1, 
+                     col= "red"))
+    with(data, lines(Timestamp, 
+                     Sub_metering_3, 
+                     type = "l", 
+                     lty = 1, 
+                     col= "blue"))
+    legend("topright", 
+           bty= legend.border,
+           legend = names(data[7:9]), 
+           lty= c(1,1,1), 
+           col = c("black", "red", "blue") )     
+}
 
+build.plot4.topright <- function(){
+    with(data,
+         plot(Timestamp,
+              Voltage, 
+              type = "n",
+              ylab = "Voltage",
+              xlab = "datetime")) 
+    with(data, lines(Timestamp, 
+                     Voltage, 
+                     type = "l", 
+                     lty = 1, 
+                     col= "black"))
+}
+
+build.plot4.bottomright <- function(){
+    with(data,
+         plot(Timestamp,
+              Global_reactive_power, 
+              type = "n",
+              xlab = "datetime")) 
+    with(data, lines(Timestamp, 
+                     Global_reactive_power, 
+                     type = "l", 
+                     lty = 1, 
+                     col= "black"))
+}
+
+
+new.plot("plot1.png")  
+build.plot1()
+dev.off()
+print(dev.cur())
+
+new.plot("plot2.png")  
+build.plot2()
+dev.off()
+
+new.plot("plot3.png")  
+build.plot3("o")
+dev.off()
+
+# constructing fourth plot
+new.plot("plot4.png")
+par(mfcol = c(2,2))
+build.plot2()
+build.plot3("n")
+build.plot4.topright()
+build.plot4.bottomright()
+
+dev.off()
